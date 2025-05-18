ac2a61eee6201e250d17f29f474be6b38d4db92f
Suneth Udayanga <suneth.dev@gmail.com>
2025-05-18 21:42:31 +0530

    version change

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld('api', {
-  saveData: (data) => ipcRenderer.send('save-data', data),
-  onSaveResult: (callback) => ipcRenderer.on('save-result', (_, result) => callback(result))
-})
